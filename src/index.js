6d4c8812e9c9acbc67cb06b32579dac7aabf6671
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-02-15 19:28:24 -0300

    Revert "Github pages configuration"
    This reverts commit c8e9374256bfada7cb9b166aa8f78fd31116dce6.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import store from "./store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import store from './store';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store} basename="/Dantas-Silva-React/">
+  <Provider store={store} basename='/repository-name/'>
       <App/>
   </Provider>
 );
