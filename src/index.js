1d371a211a5a05136b9f4a3bd2be18c4a1b56a22
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-02-15 19:35:31 -0300

    deploy config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store} basename='/repository-name/'>
+  <Provider store={store} basename="/Dantas-Silva-React/">
       <App/>
   </Provider>
 );
